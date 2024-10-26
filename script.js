49cdd1961cbc77affbfa1eba5c7ddc6d4e5f02c0
vaibhavraut07 <vaibhavraut087@gmail.com>
2024-10-26 23:01:51 +0530

    fixed bug down arrow issue with horse

--- script.js
+++ script.js
@@ -87,85 +87,95 @@ window.addEventListener('load', function() {
     }
 
   }
-  class Player{
-    constructor(game){
-      this.game = game; 
+  class Player {
+    constructor(game) {
+      this.game = game;
       this.width = 120;
-      this.height = 190; 
+      this.height = 190;
       this.x = 20;
-      this.y= 100;
-      this.frameX = 0; 
+      this.y = 100;
+      this.frameX = 0;
       this.frameY = 0;
-      this.maxFrame = 37; 
+      this.maxFrame = 37;
       this.speedY = 0;
-      this.maxSpeed = 2 ;
+      this.maxSpeed = 2;
       this.projectiles = [];
-      this.image = document.getElementById('player')
-      this.pwerUp = false; 
+      this.image = document.getElementById('player');
+      this.powerUp = false;
       this.powerTimer = 0;
-      this.powerUpLimit = 10000; 
+      this.powerUpLimit = 10000;
+    }
+  
+    update(deltaTime) {
+      if (this.game.keys.includes('ArrowUp')) {
+        this.speedY = -this.maxSpeed;
+      } else if (this.game.keys.includes('ArrowDown')) {
+        this.speedY = this.maxSpeed;
+      } else {
+        this.speedY = 0;
       }
-
-    update(deltaTime){
-      if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed; 
-      else if(this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
-      else this.speedY = 0; 
-      this.y += this.speedY
-      //vertical boundaries
-      if(this.y > this.game.height - this.height) this.y = this.game.height - this.height * 0.5; 
-      else if(this.y< -this.height * 0.5) this.y = -this.height * 0.5; 
-      //handle Projectiles
+      this.y += this.speedY;
+  
+      // Vertical boundaries
+      if (this.y > this.game.height - this.height) {
+        this.y = this.game.height - this.height;
+      } else if (this.y < 0) {
+        this.y = 0;
+      }
+  
+      // Handle projectiles
       this.projectiles.forEach(projectile => {
-        projectile.update()
-      })
-      this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion)
-      //Sprite Animation
-      if(this.frameX < this.maxFrame){
-        this.frameX++
-      }else{
-        this.frameX = 0
+        projectile.update();
+      });
+      this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+  
+      // Sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
       }
-
-      //power up
-      if(this.powerUp){
-        if(this.powerTimer > this.powerUpLimit){
-        this.powerTimer = 0;
-        this.powerUp = false;
-        this.frameY= 0
-      } else{
-        this.powerTimer += deltaTime;
-        this.frameY = 1; 
-        this.game.ammo += 0.1; 
+  
+      // Power up
+      if (this.powerUp) {
+        if (this.powerTimer > this.powerUpLimit) {
+          this.powerTimer = 0;
+          this.powerUp = false;
+          this.frameY = 0;
+        } else {
+          this.powerTimer += deltaTime;
+          this.frameY = 1;
+          this.game.ammo += 0.1;
+        }
       }
     }
-      
-    }
-    draw(context){
-      if(this.game.debug)context.strokeRect(this.x, this.y, this.width, this.height);
+  
+    draw(context) {
+      if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
       this.projectiles.forEach(projectile => {
-        projectile.draw(context)
-      }) 
-      context.drawImage(this.image, this.frameX *this.width, this.frameY *this.height, this.width, this.height,  this.x, this.y, this.width, this.height);
-      
+        projectile.draw(context);
+      });
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
-
-    shootTop(){
-      if(this.game.ammo > 0){
-        this.projectiles.push(new Projectile(this.game, this.x +80, this.y +30));
-        this.game.ammo--; 
+  
+    shootTop() {
+      if (this.game.ammo > 0) {
+        this.projectiles.push(new Projectile(this.game, this.x + 80, this.y + 30));
+        this.game.ammo--;
       }
-      if(this.powerUp) this.shootBottom()
-      
+      if (this.powerUp) this.shootBottom();
     }
-    shootBottom(){
-      if(this.game.ammo > 0){
-        this.projectiles.push(new Projectile(this.game, this.x +80, this.y +175));
+  
+    shootBottom() {
+      if (this.game.ammo > 0) {
+        this.projectiles.push(new Projectile(this.game, this.x + 80, this.y + 175));
       }
     }
-    enterPowerUp(){
-      this.powerTimer = 0; 
-      this.powerUp = true; 
-      if(this.game.ammo < this.game.maxAmmo) this.game.ammo = this.game.maxAmmo; 
+  
+    enterPowerUp() {
+      this.powerTimer = 0;
+      this.powerUp = true;
+      if (this.game.ammo < this.game.maxAmmo) this.game.ammo = this.game.maxAmmo;
     }
   }
 
